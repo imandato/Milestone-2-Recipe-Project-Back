1421723f6b9707e4964c70f49c597956a34102ea
unknown <choesenbyme@hotmail.com>
2022-10-23 02:26:47 -0400

    make mutiple controller routes

--- controllers/recipe_controllers.js
+++ controllers/recipe_controllers.js
@@ -1,57 +1,107 @@
-
 const recipe = require('express').Router()
 const db = require('../models')
 const {Recipes , Ingredients, Steps, Recipe_ingredient} = db
-const { Sequelize } = require('sequelize')
+const {Op} = require('sequelize')
 
-//INDEX
+
+//Index - show 5 recipes in database randomly (100 recipes)
 recipe.get('/', async(req,res) => {
     try{
-        const foundRecipes = await Recipes.findAll({
-            attributes:["title", "author"]
-        })
+        var foundRecipes = [];
+        while(foundRecipes.length < 5){
+            var r = Math.floor(Math.random()*100) + 1;
+            var found = Recipes.findAOne({
+                where:{recipes_id: r }
+            })
+            if(foundRecipes.indexOf(found) === -1){
+                foundRecipes.push(found);
+            }
+        }
         res.status(200).json({
-            message:"found all recipes",
+            message:"Found 5 recipes you might want!",
             data:foundRecipes
         })
     }catch(err){
         res.status(500).json(err)
     }
+    
 })
 
-//SHOW
+//SHOW-find all recipes by author
+recipe.get('/:author', async(req,res) => {
+    try{
+        const foundRecipes = await Recipes.findAll({
+            where:{author:req.params.author}}
+        )
+        res.status(200).json({
+            message:`found all recipes with author: ${req.params.author}`,
+            data:foundRecipes
+        })
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
+})
+
+//SHOW-Find all recipes by title
+recipe.get('/:title', async(req,res) => {
+    try {
+        const foundRecipe = await Recipes.findAll({
+            where: { title: req.params.title }
+        })
+        res.status(200).json({
+            message:`found all recipes with title: ${req.params.title}`,
+            data:foundRecipe})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//SHOW-Find all recipes by one ingredient's name
 recipe.get('/:name', async(req,res) => {
     try {
-        const foundRecipe = await Recipe.findOne({
-            where: { title: req.params.name }
+        const foundRecipe = await Recipes.findAll({
+            where: { name: req.params.name }
         })
-        res.status(200).json(foundRecipe)
+        res.status(200).json({
+            message:`found all recipes with ingredient: ${req.params.name}`,
+            data:foundRecipe})
     } catch (error) {
         res.status(500).json(error)
     }
    
 })
 
-//CREATE
-recipe.post('/', (req,res) => {
-    res.send(
-        'Index'
-    )
-    Recipe.push(req.body)
-    res.redirect('/recipe')
+//SHOW-find all recipes by step number
+recipe.get('/:step_num', async(req,res) => {
+    try {
+        const foundRecipe = await Recipes.findAll({
+            where: { step_number: req.params.step_num }
+        })
+        res.status(200).json({
+            message:`found all recipes with steps number: ${req.params.step_num}`,
+            data:foundRecipe})
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-//EDIT
-recipe.get('/:id', (req,res) => {
-    res.send('places/edit')
+//Filter all recipes without ingredients
+recipe.get('/:no_name', async(req,res) => {
+    try {
+        const foundRecipe = await Recipes.findAll({
+            where: { [Op.not]:{name:req.params.no_name}
+        }})
+        res.status(200).json({
+            message:`found all recipes without ingredients: ${req.params.no_name}`,
+            data:foundRecipe})
+    } catch (error) {
+        res.status(500).json(error)
+    }  
 })
 
-//DELETE
-recipe.delete('/:id', (req, res) => {
-    recipe.splice(req.params.id, 1)
-    res.status(303).redirect('/recipe')
-  })
+//Filter all recipes with some ingredients, and without some others.
 
-  
+
 
 module.exports = recipe
