b362353032d4c1fe0273f6b8260c2300d1649403
Decodedal <Palumbodallas@gmail.com>
2022-10-24 20:24:22 -0400

    seeder update

--- seeders/20221024182912-recipe_ingredients.js
+++ seeders/20221024182912-recipe_ingredients.js
@@ -5,26 +5,26 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     
     await queryInterface.bulkInsert('recipe_ingredients', [{
-      recipe_id:14,
+      recipe_id:1,
       ingredient_id:1,
       quantity:"1 Lb"
     },
     {
-      recipe_id:14,
+      recipe_id:1,
       ingredient_id:2,
       quantity:"2 Tbs"
     },
     {
-      recipe_id:14,
+      recipe_id:1,
       ingredient_id:3,
       quantity:"1 Cup"
     },
     {
-      recipe_id:15,
+      recipe_id:2,
       ingredient_id:4,
       quantity:"1"
     },
-    { recipe_id:15,
+    { recipe_id:2,
       ingredient_id:5,
       quantity:"1"}
 ], {});
