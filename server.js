3fa864a9bb0f2c6935fdbe6d02b0271ef1b905b0
Decodedal <Palumbodallas@gmail.com>
2022-10-24 21:18:22 -0400

    uptodate

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 //DEPENDENCIES
 const express = require('express')
 const app = express();
-<<<<<<< HEAD
-const { get } = require('./controllers/recipe_controllers');
-=======
 const { Sequelize } = require('sequelize');
 const cors = require('cors')
->>>>>>> 239a4f7e7a5dbfc9444d11265d20be05caa34a19
 
 //MIDDLEWARE
 require('dotenv').config()
